55c1c287130b4d935e569795b2cc15a1ff67a6d9
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2021-05-23 19:57:27 -0300

    inicio do render

---
+++ src/headers/renderer.h
@@ -0,0 +1,28 @@
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include <Windows.h>
+#include <iostream>
+
+
+class Renderer {
+private:
+    int nScreenWidth;		// Console Screen Size X (columns)
+    int nScreenHeight;		// Console Screen Size Y (rows)
+    int fDepth;
+    float fFOV;
+    // std::wstring map;
+    Map map;
+
+    wchar_t *screen;
+	HANDLE hConsole;
+	DWORD dwBytesWritten;
+
+public:
+    Renderer();
+    void render();
+
+
+};
+
+#endif
---
+++ src/source/renderer.cpp
@@ -0,0 +1,62 @@
+#include <Windows.h>
+#include "math.h"
+
+#include "headers/renderer.h"
+
+Renderer::Renderer(int nScreenWidth, int nScreenHeight, float fFOV, Map map) {
+    this->nScreenWidth = nScreenWidth;
+    this->nScreenHeight = nScreenHeight;
+    this->map = map;
+    // this->fFOV = fFOV;
+    this->fFOV = (float) 3.141592 / 3.0;
+
+    // Create Screen Buffer
+	this->screen = new wchar_t[nScreenWidth*nScreenHeight];
+	this->hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	std::SetConsoleActiveScreenBuffer(this->hConsole);
+	this->dwBytesWritten = 0;
+}
+
+Renderer::render(Player player) {
+
+    for (size_t x = 0; x < this->nScreenWidth; x++) {
+        float fRayAngle = (player.fPlayerA - this->fFOV / 2.f) + ((float) x / (float) this->nScreenHeight) * this->fFOV;
+
+        float fDistanceToWall = 0;
+
+        float fEyeX = sinf(fRayAngle);
+        float fEyeY = cosf(fRayAngle);
+
+        bool bHitWall = false;
+        while(!bHitWall) {
+            fDistanceToWall += 0.1f;
+
+            int nTestX = (int)(fPlayerA + fEyeX * fDistanceToWall);
+            int nTestY = (int)(fPlayerA + fEyeY * fDistanceToWall);
+
+            // Ray out of bounds
+            if (nTestX < 0 || nTestX >= this->map.nMapWidth || nTestY < 0 || nTestY >= this->map.nMapHeight) {
+                bHitWall = true;
+                fDistanceToWall = fDepth;
+            } else // Ray inbounds
+                if (map.smap[nTestY * this->map.nMapWidth + nTestX] != '.')
+                    bHitWall = true;
+
+            int nCeiling = (float) (this->nScreenHeight / 2) - this->nScreenHeight / (float) fDistanceToWall;
+            int nFloor = this->nScreenHeight - nCeiling;
+
+            for (int y = 0; y < this->nScreenHeight; y++) {
+                if (y < nCeiling)
+                    screen[y * this->nScreenWidth + x] = ' ';
+                else if (y > nCeiling && y > nFloor)
+                    screen[y * this->nScreenWidth + x] = '#'; // COLOR SHADING
+                else
+                    screen[y * this->nScreenWidth + x] = ' ';
+                    // 15:49
+            }
+
+
+        }
+    }
+
+}
