eeb70a5eb9a7b0117e5f7c87d4b9106eacb6576e
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2021-06-05 19:34:04 -0300

    code formatting

--- src/headers/engine.h
+++ src/headers/engine.h
@@ -13,26 +13,26 @@
 #define F_RAY_STEP_SIZE 0.1
 
 // ASCII Textures
-#define H_TEXTURE_WALL_CLOSE 0xdb
-#define H_TEXTURE_WALL_MEDIUM 0xb2
-#define H_TEXTURE_WALL_FAR 0xb1
-#define H_TEXTURE_WALL_VERY_FAR 0xb0
-#define H_TEXTURE_FLOOR_CLOSE 0xf0
-#define H_TEXTURE_FLOOR_MEDIUM 0xcd
-#define H_TEXTURE_FLOOR_FAR 0xc4
-#define H_TEXTURE_FLOOR_VERY_FAR 0xfa
-#define H_TEXTURE_EMPTY 0xff
+#define H_TEXTURE_WALL_CLOSE            0xdb
+#define H_TEXTURE_WALL_MEDIUM           0xb2
+#define H_TEXTURE_WALL_FAR              0xb1
+#define H_TEXTURE_WALL_VERY_FAR         0xb0
+#define H_TEXTURE_FLOOR_CLOSE           0xf0
+#define H_TEXTURE_FLOOR_MEDIUM          0xcd
+#define H_TEXTURE_FLOOR_FAR             0xc4
+#define H_TEXTURE_FLOOR_VERY_FAR        0xfa
+#define H_TEXTURE_EMPTY                 0xff
 
 // Commands
-#define C_WALK_FORWARD 0x57          // 'W'
-#define C_WALK_BACKWARD 0x53         // 'S'
-#define C_WALK_LEFT 0x41             // 'A'
-#define C_WALK_RIGHT 0x44            // 'D'
-#define C_LOOK_LEFT VK_LEFT          // 0x25
-#define C_LOOK_RIGHT VK_RIGHT        // 0x27
-#define C_FIRE_WEAPON VK_DOWN        // 0x28
-#define C_FIRE_WEAPON_ALT VK_SPACE   // 0x20
-#define C_LOOK_SENSI 0.5
+#define C_WALK_FORWARD                  0x57           // 'W'
+#define C_WALK_BACKWARD                 0x53           // 'S'
+#define C_WALK_LEFT                     0x41           // 'A'
+#define C_WALK_RIGHT                    0x44           // 'D'
+#define C_LOOK_LEFT                     VK_LEFT        // 0x25
+#define C_LOOK_RIGHT                    VK_RIGHT       // 0x27
+#define C_FIRE_WEAPON                   VK_DOWN        // 0x28
+#define C_FIRE_WEAPON_ALT               VK_SPACE       // 0x20
+#define C_LOOK_SENSI                    0.5            // default: 0.5
 
 
 
