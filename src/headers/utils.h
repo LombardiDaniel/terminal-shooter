8e4f6e2e041aed368a542b282abe650b29f4269a
Daniel Lombardi <lombardi.daniel.o@gmail.com>
2021-06-06 19:29:33 -0300

    grammar

--- src/headers/utils.h
+++ src/headers/utils.h
@@ -8,7 +8,7 @@ namespace utils {
 
     const float PI = 3.141592f;
 
-    template<typename T = char*>
+    template <typename T = char*>
     void _insert_color(char* original, unsigned int pos, T new_insertion) {
         for (size_t i = 0; i < 6; i++)
             original[i + pos] = new_insertion[i + pos];
